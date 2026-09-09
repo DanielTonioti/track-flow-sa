2f8f82000be7743754a0afd1a0e10855d8b48af2
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-09-09 11:54:35 -0300

    fix - links not working becuase of the renaming of files

--- public/Sensor_Data.php
+++ public/Sensor_Data.php
@@ -42,11 +42,11 @@
                                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                                     <li class="nav-item">
                                         <a class="nav-link active" aria-current="page"
-                                            href="admin.html">Funcionarios</a>
+                                            href="Admin.php">Funcionarios</a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link active" aria-current="page"
-                                            href="relatories.html">Relatórios</a>
+                                            href="Relatories.php">Relatórios</a>
                                     </li>
                                     <li class="nav-item">
                                         <a class="nav-link active" aria-current="page" href="hub.html">Sensores</a>
