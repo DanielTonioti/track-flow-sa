8fc4355dfcfb8be47d5540ef6f152d425d167d28
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-09-14 11:09:53 -0300

    fix - conexão com scripts na página de admin

--- public/Admin.php
+++ public/Admin.php
@@ -312,9 +312,9 @@
 
 
 
-    <script src="../scripts/script-users-page.js"></script>
-    <script src="../scripts/script-voltar.js"></script>
-    <script src="../scripts/script-navbar.js"></script>
+    <script src="../scripts/scriptUsersPage.js"></script>
+    <script src="../scripts/scriptVoltar.js"></script>
+    <script src="../scripts/scriptNavbar.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
         crossorigin="anonymous"></script>
