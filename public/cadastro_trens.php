29ecddec3f4ce4486da384330ce6f5f6124e27ae
DanielTonioti <daniel_tonioti@estudante.sesisenai.org.br>
2026-09-15 21:33:24 -0300

    add - adicionado lista de trens cadastrados(confundi os nomes no commit anterior)

--- public/cadastro_trens.php
+++ public/cadastro_trens.php
@@ -61,7 +61,7 @@
     </header>
 
     <main class="CadastroTremPage">
-        <h1 class="CadastroTremTitle">Cadastrar Trem</h1>
+        <h1 class="CadastroTremTitle cores-color" data-color="white">Cadastrar Trem</h1>
 
         <form class="CadastroTremForm" id="CadastroTremForm" novalidate>
             <div class="CadastroTremFields">
@@ -85,14 +85,40 @@
             </div>
         </form>
 
-        <button class="CadastroTremButton" type="submit" form="CadastroTremForm">Cadastrar</button>
+        <button class="CadastroTremButton" type="button">Cadastrar</button>
+
+        <section class="CadastroTremList cores-color" data-color="white" aria-labelledby="TituloTrilhosCadastrados">
+            <h2 id="TituloTrilhosCadastrados" class="CadastroTremListTitle">Trilhos cadastrados</h2>
+            <div class="CadastroTremListItem" data-trilho="N289T">
+                <div>
+                    <strong>N289T</strong>
+                    <span>Inicial - 1200 m</span>
+                </div>
+                <button class="CadastroTremDeleteButton" type="button" data-bs-toggle="modal"
+                    data-bs-target="#ModalExcluirTrilho" data-trilho-nome="N289T">
+                    Excluir
+                </button>
+            </div>
+        </section>
     </main>
 
-    <script src="../scripts/scriptTrilhos.js"></script>
+    <div class="modal fade CadastroTremModal" id="ModalExcluirTrilho" tabindex="-1"
+        aria-labelledby="TituloModalExcluirTrilho" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content CadastroTremModalBox cores-background cores-color" data-background="cinza-claro"
+                data-color="black">
+            <h2 id="TituloModalExcluirTrilho">Certeza que deseja<br> excluir Trem:<br>
+                <span>N289T</span>.</h2>
+            <div class="CadastroTremModalButtons">
+                <button type="button" id="ConfirmarExclusaoTrilho" data-bs-dismiss="modal">Confirmar</button>
+                <button type="button" data-bs-dismiss="modal">voltar</button>
+            </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
     <script src="../scripts/scriptNavbar.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-FKyoEForCGlyvwxHj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
-        crossorigin="anonymous"></script>
 </body>
 
 </html>
