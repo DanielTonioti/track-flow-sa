88ecdee79d085378845db15132f88c2f33e05c4f
Roberto Hille <roberto_hille@estudante.sesisenai.org.br>
2026-09-21 10:08:54 -0300

    up - pelo momento pagina de trilhos e rotas finalizada,

--- public/trilhos_rotas.php
+++ public/trilhos_rotas.php
@@ -86,10 +86,11 @@
             <h2 class="titulo-Sensor"> Gerenciar trems e sensores </h2>
         </div>
     </div> 
-        <div class="flex blockcentro">
-            <div class="cores-background p-2 rounded-4 blockcentro" data-background="azure-claro-fundo">
-                <div>
-                    <div class="table-trems">
+    <div class="blockcentro">
+        <div class="flex">
+            <div>
+            <div class="cores-background p-2 rounded-4 blockcentro table-trems" data-background="azure-claro-fundo">
+                    <div>
                         <table class="table">
                             <tr>
                                 <th>ID</th>
@@ -100,11 +101,11 @@
                             </tr>
                         </table>
                     </div>
-                </div>
+                    <a href="cadastro_trens.php"> Cadastrar trem </a>
             </div>
-
+            </div>
+            <div>
             <div class="cores-background p-2 rounded-4 blockcentro" data-background="azure-claro-fundo">
-                <div>
                     <table class="table table-trilhos">
                         <tr>
                             <th>ID</th>
@@ -112,13 +113,15 @@
                             <th>Tamanho</th>
                         </tr>
                     </table>
-                </div>
+            <a href="cadastro_linhas.php"> Cadastrar linha </a>
             </div>
-        </div> 
+            </div>
+        </div>
+    </div> 
 
    
-   <a href="cadastro_trens.php"> Cadastrar trem </a>
-   <a href="cadastro_linhas.php"> Cadastrar linha </a>
+   
+   
    
    </main>
 
